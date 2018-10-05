8b76ce9dea7dbd757cd21c5f729010a7761f9e69
NamelessClown <bilal.bahjaoui@student.ehb.be>
2018-10-05 13:07:53 +0200

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,12 @@
+<?php
+
+	$text = "Hello world, this is my first Alexa Skill bitch";
+
+	$array = array("version" => "1.0","response" => array("outputSpeech"=>array("type"=>"PlainText","text"=>$text)));
+
+	echo json_encode($array);
+
+
+
+
+?>
