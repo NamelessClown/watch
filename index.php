2b7e2c3a53f2b8689900687502d86f4670cea49b
NamelessClown <bilal.bahjaoui@student.ehb.be>
2018-10-06 14:34:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
 	$text = "Hello world, this is my first Alexa Skill";
+	$echoRequest   = json_decode(file_get_contents('php://input'));
 
 
 
 
-
-	$array = array("version" => "1.0","response" => array("outputSpeech"=>array("type"=>"PlainText","text"=>$text)));
+	$array = array("version" => "1.0","response" => array("outputSpeech"=>array("type"=>"PlainText","text"=>$echoRequest)));
 
 	echo json_encode($array);
 
