cae06b6480435c5749553a539202443e897ff320
NamelessClown <bilal.bahjaoui@student.ehb.be>
2018-10-06 14:38:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 	$text = "Hello world, this is my first Alexa Skill";
 	$echoRequest   = json_decode(file_get_contents('php://input'));
+	$whatiwant   = $echoRequest->request->intent->slots->color->value;
 
 
 
-
-	$array = array("version" => "1.0","response" => array("outputSpeech"=>array("type"=>"PlainText","text"=>$echoRequest)));
+	$array = array("version" => "1.0","response" => array("outputSpeech"=>array("type"=>"PlainText","text"=>$whatiwant)));
 
 	echo json_encode($array);
 
